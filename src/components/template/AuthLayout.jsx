47e691926159492ceeb33d5e79f09f609d110362
putraanggar562 <anggarap208@gmail.com>
2026-01-28 20:37:13 +0700

    fix

--- src/components/template/AuthLayout.jsx
+++ src/components/template/AuthLayout.jsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[url(../../public/img/bacground.jpg)] bg-cover bg-opacity-50 backdrop-blur">
+    <div className="flex items-center justify-center min-h-screen bg-[url(../../public/img/background.jpg)] bg-cover bg-opacity-50 backdrop-blur">
       <div className="bg-black bg-opacity-70 text-white p-8 rounded-xl w-full max-w-md shadow-lg">
         {children}
       </div>
