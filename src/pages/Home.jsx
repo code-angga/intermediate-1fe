b1112a00a60dc470070d403e326cf994c0978399
putraanggar562 <anggarap208@gmail.com>
2026-01-28 20:37:13 +0700

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,24 @@
-// pages/Home.jsx
-import Card from "../components/Card";
-import TrailerModal from "../components/TrailerModal";
-import { useSelector } from "react-redux";
+// // pages/Home.jsx
+// import Card from "../components/Card";
+// import TrailerModal from "../components/TrailerModal";
+// import { useSelector } from "react-redux";
 
-const Home = () => {
-  const { melanjutkan, trending, topRated, upcoming } = useSelector(
-    (state) => state.movies,
-  );
+// const Home = () => {
+//   const { melanjutkan, trending, topRated, upcoming } = useSelector(
+//     (state) => state.movies,
+//   );
 
-  return (
-    <>
-      <Card movies={melanjutkan} />
-      <Card movies={trending} />
-      <Card movies={topRated} />
-      <Card movies={upcoming} />
+//   return (
+//     <>
+//       <Card movies={melanjutkan} />
+//       <Card movies={trending} />
+//       <Card movies={topRated} />
+//       <Card movies={upcoming} />
 
-      {/* GLOBAL TRAILER */}
-      <TrailerModal />
-    </>
-  );
-};
+//       {/* GLOBAL TRAILER */}
+//       <TrailerModal />
+//     </>
+//   );
+// };
 
-export default Home;
+// export default Home;
